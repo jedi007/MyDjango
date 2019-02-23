e5b4905ab5090f267d6cbeca6d13e7abb393e952
jedi007 <360751194@qq.com>
2019-02-23 21:11:15 +0800

    add Mydjango code

---
+++ MyDjango/gobang/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,re_path#导入re_path即可匹配正则
+
+from . import views
+
+app_name='gobang'#在django2 中必须添加appname,否则urls.py中无法使用include包含namespace
+
+urlpatterns = [
+    path('index/', views.index), 
+]
